b9ffbfc9b25cf88ee19bb2adce870c33054161c8
brucehan <intragerhan@gmail.com>
2024-11-28 22:28:52 +0900

    product list

--- e-commerce/ecommerce/src/main/java/com/ecommerce/repository/CommerceDAO.java
+++ e-commerce/ecommerce/src/main/java/com/ecommerce/repository/CommerceDAO.java
@@ -1,13 +1,16 @@
 package com.ecommerce.repository;
 
+import com.ecommerce.entity.Product;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
-public class ConnectionDAO {
+public class CommerceDAO {
     // DB 연결 -> config.xml (환경설정파일) -> API read 연결작업을 대신 해주면 된다.
     private static SqlSessionFactory sqlSessionFactory; // Connection(SqlSession) Pool
     static {
@@ -18,5 +21,12 @@ public class ConnectionDAO {
         } catch (IOException e) {
             e.fillInStackTrace();
         }
+    }
+
+    public List<Product> getProductList() {
+        SqlSession session = sqlSessionFactory.openSession();
+        List<Product> list = session.selectList("productList");
+        session.close();
+        return list;
     }
 }
