749d511c188d06a064e0878859aacaa0f6506201
brucehan <intragerhan@gmail.com>
2024-11-30 21:25:54 +0900

    add product to cart

--- e-commerce/ecommerce/src/main/java/com/ecommerce/controller/TemplateController.java
+++ e-commerce/ecommerce/src/main/java/com/ecommerce/controller/TemplateController.java
@@ -1,10 +1,12 @@
 package com.ecommerce.controller;
 
-import java.io.*;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/")
 public class TemplateController extends HttpServlet {
